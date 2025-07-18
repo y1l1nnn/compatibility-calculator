69196d35cfd48ff40e35a9c3318613ee28473cd4
Elaine Zhang <cmddm39@gmail.com>
2025-07-18 21:49:12 +1000

    updated server.ts and attr_compatibility.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 import {Person} from './types'
 
 const app = express();
@@ -8,13 +10,36 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+// read attr_compatibility.json 
+const pathname = path.join(__dirname, 'compatibility.json');
+const data = fs.readFileSync(pathname, 'utf-8');
+const compatibilityData = JSON.parse(data);
+
+// get score of given attribute 
 function getScore(attr: keyof Person, a: string, b: string): number {
-    return 1
+    const score = compatibilityData[attr][a][b];
+    return score;
 }
 
+// endpoint to calculate results 
 app.post('/api/calculate', (req, res) => {
     const {personA, personB} = req.body 
-    res.json({})
+    const initialsScore = 0;
+    const starScore = getScore("starSign", personA.starSign, personB.starSign);
+    const mbtiScore = getScore("mbti", personA.mbti, personB.mbti);
+    const zodiacScore = getScore("zodiac", personA.zodiac, personB.zodiac); 
+    const bloodScore = getScore("blood", personA.blood, personB.blood);  
+    const finalScore = Math.round(initialsScore * 0.1 + starScore * 0.3 + mbtiScore * 0.4 + zodiacScore * 0.1 + bloodScore * 0.1);
+    res.json({ 
+        score: finalScore,
+        breakdown: {
+            initials: initialsScore,
+            starSign: starScore,
+            mbti: mbtiScore,
+            zodiac: zodiacScore, 
+            blood: bloodScore
+        }
+    });
 });
 
 app.listen(port, () => {
